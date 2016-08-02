eb7ee9e49237f0c63dad217d7d0afe4027cec2b0
chenyk0317 <chenyk0317@163.com>
2016-08-02 10:45:59 +0800

    RN嵌入

---
+++ ios/TestRN/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestRN
+//
+//  Created by chenyk on 16/8/1.
+//  Copyright © 2016年 58daojia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios/TestRN/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestRN
+//
+//  Created by chenyk on 16/8/1.
+//  Copyright © 2016年 58daojia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
