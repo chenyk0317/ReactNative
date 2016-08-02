696ea320e0ca9fe91f7447d71500402f8d7b9693
chenyongkun <chenyongkun@daojia.com>
2016-08-02 10:45:59 +0800

    RN嵌入

---
+++ ios/TestRN/ViewController.m
@@ -0,0 +1,39 @@
+//
+//  ViewController.m
+//  TestRN
+//
+//  Created by chenyk on 16/8/1.
+//  Copyright © 2016年 58daojia. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <React/RCTRootView.h>
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+- (IBAction)action:(id)sender {
+    
+    NSURL *jsCodeLocation = [NSURL
+                             URLWithString:@"http://10.253.11.117:8081/index.ios.bundle?platform=ios"];
+    RCTRootView *rootView =
+    [[RCTRootView alloc] initWithBundleURL : jsCodeLocation
+                         moduleName        : @"TestRN"
+                         initialProperties :nil launchOptions: nil];
+    UIViewController *vc = [[UIViewController alloc] init];
+    vc.view = rootView;
+    [self presentViewController:vc animated:YES completion:nil];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ ios/TestRN/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestRN
+//
+//  Created by chenyk on 16/8/1.
+//  Copyright © 2016年 58daojia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
